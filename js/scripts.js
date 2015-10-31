a602098c6a170fc7ffcedb95e4081a61a524c343
Stephen <stephenherko@gmail.com>
2015-10-31 14:48:27 -0500

    fixed header nav on scroll

--- js/scripts.js
+++ js/scripts.js
@@ -14,10 +14,53 @@ $(document).ready(function(){
   });
 
   // Mobile Btn
-
   $('.mobile-btn').click(function(){
     $('.header-nav').toggle();
     return false;
   });
+
+
+  // Show header nav on scroll down
+  var lastScrollTop = 0;
+
+  $(window).scroll(function(){
+    console.log($(this).scrollTop());
+
+    if($(this).scrollTop() >= 500){
+      $('.header').fadeIn(500);
+      $('.header').addClass('header-fixed');
+      // Account for missing space after .header-fixed
+      $('body').css({
+        'paddingTop': '113px'
+      });
+    } else {
+      $('.header').show();
+      $('.header').removeClass('header-fixed');
+      $('body').css({
+        'paddingTop': '0'
+      });
+    }
+  });
+
+  // Show header on scroll up - remove on scroll down
+  // var lastScrollTop = 0;
+  //
+  // $(window).scroll(function(event){
+  //   var scrollT = $(this).scrollTop();
+  //
+  //   if (scrollT > lastScrollTop){
+  //     // downscroll
+  //     // $('.header').fadeOut(300);
+  //     $('.header').removeClass('header-fixed');
+  //   } else {
+  //     // upscroll
+  //     // $('.header').fadeIn(300);
+  //     $('.header').addClass('header-fixed');
+  //   }
+  //
+  //   lastScrollTop = scrollT;
+  //
+  //
+  // });
 
 });
