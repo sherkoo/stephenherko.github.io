c8bb5383d6c960d4f0784cad6d7e25d6335aa170
Stephen <stephenherko@gmail.com>
2016-04-23 10:23:10 -0500

    image preloader

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -1,3 +1,7 @@
+function fadeIn() {
+  $(this).fadeIn(1000);
+}
+
 $(document).ready(function(){
 
   // Mobile Btn
@@ -22,51 +26,9 @@ $(document).ready(function(){
   });
 
   // Fade in images individually
-  function fadeIn(obj) {
-    $(obj).fadeIn(1000);
-  }
+  // function fadeIn(obj) {
+  //   $(obj).fadeIn(1000);
+  // }
 
-
-  // // Show header nav on scroll down
-  // var lastScrollTop = 0;
-  //
-  // $(window).scroll(function(){
-  //   console.log($(this).scrollTop());
-  //
-  //   if($(this).scrollTop() >= 500){
-  //     $('.header').addClass('header-fixed');
-  //     // Account for missing space after .header-fixed
-  //     $('body').css({
-  //       'paddingTop': '113px'
-  //     });
-  //   } else {
-  //     $('.header').show();
-  //     $('.header').removeClass('header-fixed');
-  //     $('body').css({
-  //       'paddingTop': '0'
-  //     });
-  //   }
-  // });
-
-  // Show header on scroll up - remove on scroll down
-  // var lastScrollTop = 0;
-  //
-  // $(window).scroll(function(event){
-  //   var scrollT = $(this).scrollTop();
-  //
-  //   if (scrollT > lastScrollTop){
-  //     // downscroll
-  //     // $('.header').fadeOut(300);
-  //     $('.header').removeClass('header-fixed');
-  //   } else {
-  //     // upscroll
-  //     // $('.header').fadeIn(300);
-  //     $('.header').addClass('header-fixed');
-  //   }
-  //
-  //   lastScrollTop = scrollT;
-  //
-  //
-  // });
 
 });
