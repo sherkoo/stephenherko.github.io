c95ab1800b5c034331978961cf9765e52a141969
Stephen <stephenherko@gmail.com>
2016-07-31 02:09:38 -0500

    image fade in test

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
+
+
 $(document).ready(function(){
+
+  
 
   // Mobile Btn
   $('.mobile-btn').click(function(){
@@ -21,22 +25,22 @@ $(document).ready(function(){
     }
   });
 
-  $('.work').imagesLoaded()
-  .always( function( instance ) {
-    console.log('all images loaded');
-  })
-  .done( function( instance ) {
-    console.log('all images successfully loaded');
-  })
-  .fail( function() {
-    console.log('all images loaded, at least one is broken');
-  })
-  .progress( function( instance, image ) {
-    var result = image.isLoaded ? 'loaded' : 'broken';
-    console.log( 'image is ' + result + ' for ' + image.img.src );
-    var $item = $( image.img );
-    $item.addClass('is-loaded');
-  });
+  // $('.work').imagesLoaded()
+  // .always( function( instance ) {
+  //   console.log('all images loaded');
+  // })
+  // .done( function( instance ) {
+  //   console.log('all images successfully loaded');
+  // })
+  // .fail( function() {
+  //   console.log('all images loaded, at least one is broken');
+  // })
+  // .progress( function( instance, image ) {
+  //   var result = image.isLoaded ? 'loaded' : 'broken';
+  //   console.log( 'image is ' + result + ' for ' + image.img.src );
+  //   var $item = $( image.img );
+  //   $item.addClass('is-loaded');
+  // });
 
   // grab list of all image thumbs
   // $('.work-grid-item-img').each(function(){
