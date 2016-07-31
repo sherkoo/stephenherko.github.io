d760b003bf55ba96e04f9ab2c7efaa274ccd2514
Stephen <stephenherko@gmail.com>
2016-07-31 02:02:02 -0500

    testing plugin

--- _site/js/scripts.js
+++ _site/js/scripts.js
@@ -21,9 +21,21 @@ $(document).ready(function(){
     }
   });
 
-  $('.work').imagesLoaded( function() {
-    // images have loaded
-    console.log('imagse have loaded');
+  $('.work').imagesLoaded()
+  .always( function( instance ) {
+    console.log('all images loaded');
+  })
+  .done( function( instance ) {
+    console.log('all images successfully loaded');
+  })
+  .fail( function() {
+    console.log('all images loaded, at least one is broken');
+  })
+  .progress( function( instance, image ) {
+    var result = image.isLoaded ? 'loaded' : 'broken';
+    console.log( 'image is ' + result + ' for ' + image.img.src );
+    var $item = $( image.img );
+    $item.addClass('is-loaded');
   });
 
   // grab list of all image thumbs
